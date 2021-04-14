5a0bbb0164ca8cf362225c34ad72c9c42cff7eef
harshvardhan106 <82279099+harshvardhan106@users.noreply.github.com>
2021-04-14 22:38:28 +0530

    Add files via upload

---
+++ postive no. in range.py
@@ -0,0 +1,23 @@
+# list of numbers
+list1 = [12, -7, 5, 64, -14]
+
+# iterating each number in list
+for num in list1:
+	
+    # checking condition
+    if num >= 0:
+       print(num, end = " ")
+
+
+
+# list of numbers
+list2 = [12, 3, 14, 64, -95]
+
+# iterating each number in list
+for number in list2:
+	
+    # checking condition
+    if number >= 0:
+       print(number, end = " ")
+       
+    
